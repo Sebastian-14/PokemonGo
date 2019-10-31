2edd9de7a6e82a5be9968e19a308de8f4a0d7be2
Sebastian <sebastian.sanchez@tecsup.edu.pe>
2019-10-31 12:59:59 -0500

    first commit

---
+++ PokemonGo/PokedexViewController.swift
@@ -0,0 +1,14 @@
+
+import UIKit
+
+class PokedexViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
--- PokemonGo/ViewController.swift
+++ PokemonGo/ViewController.swift
@@ -7,14 +7,59 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    var ubicacion = CLLocationManager()
+    
+    var contActualizaciones = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        ubicacion.delegate = self
+        
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            mapView.showsUserLocation = true
+            ubicacion.startUpdatingLocation()
+            Timer.scheduledTimer(withTimeInterval: 5, repeats: true, block: {(timer) in
+                if let coord = self.ubicacion.location?.coordinate{
+                    let pin = MKPointAnnotation()
+                    pin.coordinate = coord
+                    let randomLat = (Double(arc4random_uniform(200))-100.0)/5000.0
+                    let randomLon = (Double(arc4random_uniform(200))-100.0)/5000.0
+                    pin.coordinate.latitude += randomLat
+                    pin.coordinate.longitude += randomLon
+                    self.mapView.addAnnotation(pin)
+                }
+            })
+        } else {
+            ubicacion.requestWhenInUseAuthorization()
+        }
     }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        //print("ubicación actualizada")
+        if(contActualizaciones < 1){
+            let region = MKCoordinateRegion(center: ubicacion.location!.coordinate, latitudinalMeters: 1000, longitudinalMeters: 1000)
+            mapView.setRegion(region, animated: true)
+            contActualizaciones += 1
+        } else {
+            ubicacion.stopUpdatingLocation()
+        }
+    }
+    
 
-
+    @IBAction func centrarTapped(_ sender: Any) {
+        if let coord = ubicacion.location?.coordinate {
+            let region = MKCoordinateRegion(center: coord, latitudinalMeters: 1000, longitudinalMeters: 1000)
+            mapView.setRegion(region, animated: true)
+            contActualizaciones += 1
+        }
+    }
+    
+    
+    
 }
 
